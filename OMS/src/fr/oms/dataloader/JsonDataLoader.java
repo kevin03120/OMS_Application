01d8be866df0361b95df54118fa8a3a8b134da03
Quentin <quentinseptier@gmail.com>
2015-07-08 12:17:28 +0200

    Ajout de l'overlay pour le tuto sur la page d'accueil
    Ajout d'un timeout lorsqu'il n'y a pas de réponse http

--- OMS/src/fr/oms/dataloader/JsonDataLoader.java
+++ OMS/src/fr/oms/dataloader/JsonDataLoader.java
@@ -14,33 +14,33 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-
-import fr.oms.activities.Activity_Chargement;
-import fr.oms.activities.R;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import fr.oms.activities.Activity_Chargement;
 import fr.oms.activities.MainActivity;
+import fr.oms.activities.R;
 
 public class JsonDataLoader extends AsyncTask<Context, Void, Void> implements iLoadData  {
-	
+
 	private static JsonDataLoader instance=null;
 	private Map<String,String> urlsFichiers;
 	private Context context=null;
 	private ProgressBar barre=null;
 	private TextView txtInfo;
-	
+	private HttpURLConnection connect;
+
 	public JsonDataLoader(Context context, ProgressBar bar, TextView txtTitre) {
 		barre=bar;
 		this.context=context;
 		this.txtInfo=txtTitre;
-		
+
 		urlsFichiers = new HashMap<String, String>();
 		urlsFichiers.put("actus.json", "http://www.oms-clermont-ferrand.fr/api/v1/actus.json");
 		urlsFichiers.put("evenements.json", "http://www.oms-clermont-ferrand.fr/api/v1/evenements.json");
@@ -54,27 +54,37 @@ public class JsonDataLoader extends AsyncTask<Context, Void, Void> implements iL
 		}
 		return instance;
 	}	
-	
+
 
 
 	@Override
 	protected void onPreExecute() {
 		super.onPreExecute();		
 	}
-	
+
 	@Override
 	protected Void doInBackground(Context... params) {
+
+		try{
+			if(!isCancelled()){
+				loadAllFileFromServer(params[0]);
+			}else{
+				connect.disconnect();
+			}
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
 		
-		loadAllFileFromServer(params[0]);
-					
-//		JSONArray array=null;
-//		try {
-//			array=obj.getJSONArray("result");
-//			System.out.println(array.getJSONObject(0).getString("title"));
-//		} catch (JSONException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		
+
+		//		JSONArray array=null;
+		//		try {
+		//			array=obj.getJSONArray("result");
+		//			System.out.println(array.getJSONObject(0).getString("title"));
+		//		} catch (JSONException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
 		return null;
 	}
 
@@ -86,7 +96,7 @@ public class JsonDataLoader extends AsyncTask<Context, Void, Void> implements iL
 		context.startActivity(intent);
 		Activity_Chargement.actiCharg.finish();
 	}
-	
+
 	private void writeInLocalFile(FileOutputStream f, JSONObject jsonObj) {
 		try {
 			f.write(jsonObj.toString().getBytes());
@@ -95,25 +105,33 @@ public class JsonDataLoader extends AsyncTask<Context, Void, Void> implements iL
 		}
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public void loadAllFileFromServer(Context context) {
 		int cpt = 25;
 		for(Map.Entry<String, String> entry : urlsFichiers.entrySet()){
 			InputStream is=null;
-			HttpURLConnection connect=null;
+			connect=null;
 			URL url = null;
 			JSONObject jsonObj=null;
+
 			try {
 				url = new URL(entry.getValue());
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
-			try {
+			try{
 				connect = (HttpURLConnection) url.openConnection();
+				connect.setConnectTimeout(2000);
 				is = connect.getInputStream();
 				//System.out.println(connect.getResponseCode());
-			} catch (IOException e) {
-				e.printStackTrace();
+			} catch (java.net.SocketTimeoutException e) {
+				Intent intent=new Intent(this.context, MainActivity.class);
+				this.context.startActivity(intent);
+				Activity_Chargement.actiCharg.finish();				
+				
+			} catch (java.io.IOException e) {
+				
 			}
 			String contentAsString = convertStreamToString(is);
 			try {
@@ -192,6 +210,8 @@ public class JsonDataLoader extends AsyncTask<Context, Void, Void> implements iL
 		}
 		return sb.toString();
 	}
-
 	
+	
+
+
 }
