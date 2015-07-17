0c962a2dd8c2f8d6ccda96f70a2349b842ca1bc1
Kévin <kevin.malleret03@gmail.com>
2015-07-17 14:22:14 +0200

    ajout view holder

--- OMS/src/fr/oms/adapter/NavDrawerListAdapter.java
+++ OMS/src/fr/oms/adapter/NavDrawerListAdapter.java
@@ -17,71 +17,93 @@ import fr.oms.modele.NavDrawerItem;
 
 public class NavDrawerListAdapter extends BaseAdapter {
 
-    private Context context;
-    private ArrayList<NavDrawerItem> navDrawerItems;
+	private Context context;
+	private ArrayList<NavDrawerItem> navDrawerItems;
 
-    public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems){
-        this.context = context;
-        this.navDrawerItems = navDrawerItems;
-    }
+	public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems){
+		this.context = context;
+		this.navDrawerItems = navDrawerItems;
+	}
 
-    @Override
-    public int getCount() {
-        return navDrawerItems.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return navDrawerItems.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @SuppressLint("InflateParams")
 	@Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            LayoutInflater mInflater = LayoutInflater.from(context);
-            convertView = mInflater.inflate(R.layout.drawer_nav_item, null);
-        }
-        LinearLayout layout = (LinearLayout)convertView.findViewById(R.id.layout_drawer);
-        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.ivIcon);
-        TextView txtTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        txtTitle.setText(navDrawerItems.get(position).getTitle());
-        afficherIcon(position, layout, imgIcon);
-        Resources r = context.getResources();
-        if((txtTitle.getText()==r.getString(R.string.accueil_underline))||(txtTitle.getText()==r.getString(R.string.annuaire_underline))||(txtTitle.getText()==r.getString(R.string.agenda_underline))){
-            txtTitle.setTextAppearance(context, R.style.TextItemNavTitre);           
-        }
-        else{
-            txtTitle.setTextAppearance(context, R.style.TextItemNavSousTitre);
-            if((txtTitle.getText()==r.getString(R.string.localise_association))||(txtTitle.getText()==r.getString(R.string.localise_equipement))||(txtTitle.getText()==r.getString(R.string.localise_discipline))){
-            	txtTitle.setTextSize(12);
-            }
-        }
-        if(txtTitle.getText()==r.getString(R.string.geolocalisation_underline)){
-        	txtTitle.setTextAppearance(context, R.style.TextItemNavTitreGeoloc);
-        }
-        if((txtTitle.getText()==r.getString(R.string.parametres))||(txtTitle.getText()==r.getString(R.string.Guide_Sport))||(txtTitle.getText()==r.getString(R.string.mentions))){
-        	layout.setBackgroundResource(navDrawerItems.get(position).getmColor());
-        	imgIcon.setVisibility(View.INVISIBLE);
-        }
-        return convertView;
-    }
+	public int getCount() {
+		return navDrawerItems.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return navDrawerItems.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	static class ViewHolderItem {
+		LinearLayout layout;
+		ImageView imgIcon;
+		TextView txtTitle;
+	}
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolderItem viewHolder;
+		if (convertView == null) {
+			LayoutInflater mInflater = LayoutInflater.from(context);
+			convertView = mInflater.inflate(R.layout.drawer_nav_item, null);
+
+			viewHolder = new ViewHolderItem();
+			viewHolder.layout = (LinearLayout)convertView.findViewById(R.id.layout_drawer);
+			viewHolder.imgIcon = (ImageView) convertView.findViewById(R.id.ivIcon);
+			viewHolder.txtTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+			viewHolder.txtTitle.setText(navDrawerItems.get(position).getTitle());
+			afficherIcon(position, viewHolder.layout, viewHolder.imgIcon);
+			Resources r = context.getResources();
+			if((viewHolder.txtTitle.getText()==r.getString(R.string.accueil_underline))||(viewHolder.txtTitle.getText()==r.getString(R.string.annuaire_underline))||(viewHolder.txtTitle.getText()==r.getString(R.string.agenda_underline))){
+				viewHolder.txtTitle.setTextAppearance(context, R.style.TextItemNavTitre);           
+			}
+			else{
+				viewHolder.txtTitle.setTextAppearance(context, R.style.TextItemNavSousTitre);
+				if((viewHolder.txtTitle.getText()==r.getString(R.string.localise_association))||(viewHolder.txtTitle.getText()==r.getString(R.string.localise_equipement))||(viewHolder.txtTitle.getText()==r.getString(R.string.localise_discipline))){
+					viewHolder.txtTitle.setTextSize(12);
+				}
+			}
+			if(viewHolder.txtTitle.getText()==r.getString(R.string.geolocalisation_underline)){
+				viewHolder.txtTitle.setTextAppearance(context, R.style.TextItemNavTitreGeoloc);
+			}
+			if((viewHolder.txtTitle.getText()==r.getString(R.string.parametres))||(viewHolder.txtTitle.getText()==r.getString(R.string.Guide_Sport))||(viewHolder.txtTitle.getText()==r.getString(R.string.mentions))){
+				viewHolder.layout.setBackgroundResource(navDrawerItems.get(position).getmColor());
+				viewHolder.imgIcon.setVisibility(View.INVISIBLE);
+			}
+			convertView.setTag(viewHolder);
+
+		}
+		else{
+			viewHolder = (ViewHolderItem) convertView.getTag();
+		}
+		NavDrawerItem objectItem = (NavDrawerItem) getItem(position);
+		    if(objectItem != null) {
+		        viewHolder.txtTitle.setText(objectItem.getTitle());
+		        viewHolder.imgIcon.setImageResource(objectItem.getIcon());
+		        viewHolder.layout.setBackgroundResource(objectItem.getmColor());
+		    }
+		 
+
+		return convertView;
+	}
 
 	private void afficherIcon(int position, LinearLayout layout,
 			ImageView imgIcon) {
 		if(navDrawerItems.get(position).getIcon()!=0){
-        	   imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
-               Double f=0.7;
-               layout.setBackgroundResource(navDrawerItems.get(position).getmColor());
-               if(navDrawerItems.get(position).getIcon()==R.drawable.ic_arrow4){
-               	imgIcon.setScaleX(f.floatValue());
-               	imgIcon.setScaleY(f.floatValue());
-               }
-        }
+			imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+			Double f=0.7;
+			layout.setBackgroundResource(navDrawerItems.get(position).getmColor());
+			if(navDrawerItems.get(position).getIcon()==R.drawable.ic_arrow4){
+				imgIcon.setScaleX(f.floatValue());
+				imgIcon.setScaleY(f.floatValue());
+			}
+		}
 	}
 }
